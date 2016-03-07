6c23bccc90cf0ec1cf0e31bf1e619ed0d4f2ced0
Shell.Xu <shell909090@gmail.com>
2016-03-07 12:58:16 +0800

    + add dictcn.py

---
+++ dictcn.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2016-03-07
+@author: Shell.Xu
+@copyright: 2016, Shell.Xu <shell909090@gmail.com>
+@license: BSD-3-clause
+
+Copyright (C) 2016 Shell Xu
+
+@comment:
+
+在dict.cn上查询关键词，并解析返回结果，以文本界面的方式提供。
+
+注：返回内容的版权归dict.cn所有。
+
+'''
+import os, sys
+import cStringIO
+import requests
+from bs4 import BeautifulSoup
+
+def currying(func, param):
+    def inner(*params):
+        return func(param, *params)
+    return inner
+
+def write_node(stream, node, strip=True):
+    stream.write(node.get_text(strip=strip).encode('utf-8')+'\n')
+
+def query_dict(words):
+    resp = requests.get('http://dict.cn/' + words)
+    if resp.status_code != 200:
+        raise Exception(resp.status_code)
+
+    doc = BeautifulSoup(resp.content)
+    unfind = doc.find('div', class_='unfind')
+    if unfind is not None:
+        return unfind.get_text()
+
+    output = cStringIO.StringIO()
+    write = currying(write_node, output)
+    map(write, doc.find('div', class_='phonetic').find_all('span'))
+    map(write,
+        filter(lambda li: 'style' not in li.attrs,
+               doc.find('ul', class_='dict-basic-ul').find_all('li')))
+    map(write, doc.find('div', class_='shape').find_all('span'))
+    return output.getvalue().strip()
+
+def main():
+    ''' dictcn.py [-h] word '''
+    optlist, argv = getopt.getopt(sys.argv[1:], 'h')
+    optdict = dict(optlist)
+    if '-h' in optdict:
+        print main.__doc__
+
+    print query_dict(argv[0])
+
+if __name__ == '__main__': main()
