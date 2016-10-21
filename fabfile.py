0545ce56f314bfd9b63ce01a1cf2fb8169ba3a38
Xu ZhiXiang (Shell.E.Xu) <shell909090@gmail.com>
2016-10-22 01:21:41 +0800

    * update license
    Signed-off-by: Xu ZhiXiang (Shell.E.Xu) <shell909090@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 '''
 @date: 2015-08-13
-@author: shell.xu
+@author: Shell.Xu
+@copyright: 2016, Shell.Xu <shell909090@gmail.com>
+@license: BSD-3-clause
 '''
 from __future__ import absolute_import, division,\
     print_function, unicode_literals
