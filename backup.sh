4bf7269d2bd61236363bb513396236d48a2bd05b
Shell.Xu <shell909090@gmail.com>
2017-09-17 00:52:21 +0800

    bug fix

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f "$1.tar.gz.gpg"
-ssh "$1" "rm -f backup.tar.gz"
+ssh "$1" "rm -f backup.tar"
 ssh "$1" "sudo bash -c 'while read i; do tar uf backup.tar \$i; done' < configs"
 ssh "$1" "sudo chown $SUDO_UID.$SUDO_GID backup.tar"
 ssh "$1" "gzip -c backup.tar" | gpg2 -e > "$1".tar.gz.gpg
