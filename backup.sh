36fdf29e7768c7d1564eeea49b6c21de6ea5f05e
Shell.Xu <shell909090@gmail.com>
2018-08-30 00:47:55 +0800

    update backup script

--- backup.sh
+++ backup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 rm -f "$1.tar.gz.gpg"
-ssh "$1" "rm -f backup.tar"
-ssh "$1" "sudo bash -c 'while read i; do tar uf backup.tar \$i; done' < configs"
-ssh "$1" "sudo chown $SUDO_UID.$SUDO_GID backup.tar"
-ssh "$1" "gzip -c backup.tar" | gpg2 -e > "$1".tar.gz.gpg
-# ssh "$1" "gzip -c backup.tar" > "$1".tar.gz
+ssh "$1" <<EOF
+rm -f backup.tar
+apt-mark showmanual > apt-mark-manual.lst
+dpkg -l > dpkg.lst
+tar uf backup.tar apt-mark-manual.lst dpkg.lst service configs
+rm -f dpkg.lst apt-mark-manual.lst
+sudo bash -c 'while read i; do tar uf backup.tar \$i; done' < configs
+sudo chown $SUDO_UID.$SUDO_GID backup.tar
+EOF
+ssh "$1" "gzip -c backup.tar" | gpg -e > "$1".tar.gz.gpg
 ssh "$1" "rm -f backup.tar"
