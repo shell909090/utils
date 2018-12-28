0fae567125f6bb9bf7eddffeeb12209ec59ff97b
Shell.Xu <shell909090@gmail.com>
2018-12-28 23:17:11 +0800

    * use temp dir * add backup.sh support

--- backup.sh
+++ backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 rm -f "$1.tar.gz.gpg"
+TMPDIR=`ssh "$1" "mktemp -d"`
+echo "create tempdir $TMPDIR"
 ssh "$1" <<EOF
-rm -f backup.tar
+cd $TMPDIR
 apt-mark showmanual > apt-mark-manual.lst
 dpkg -l > dpkg.lst
-tar uf backup.tar apt-mark-manual.lst dpkg.lst service configs
-rm -f dpkg.lst apt-mark-manual.lst
-sudo bash -c 'while read i; do tar uf backup.tar \$i; done' < configs
+tar uf backup.tar apt-mark-manual.lst dpkg.lst ~/service ~/configs
+sudo bash -c 'while read i; do tar uf backup.tar \$i; done' < ~/configs
+test -x ~/backup.sh && tar uf backup.tar ~/backup.sh
+test -x ~/backup.sh && ~/backup.sh
 sudo chown $SUDO_UID.$SUDO_GID backup.tar
 EOF
-ssh "$1" "gzip -c backup.tar" | gpg -e > "$1".tar.gz.gpg
-ssh "$1" "rm -f backup.tar"
+ssh "$1" "gzip -c $TMPDIR/backup.tar" | gpg -e > "$1.tar.gz.gpg"
+ssh "$1" "rm -rf \"$TMPDIR\""
