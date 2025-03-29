f4a20acb95fd05193c90f1c2bcd4f096ab4d6ed3
Shell.Xu <shell@testany.io>
2025-03-29 21:56:25 +0800

    add more details on ask_ollama

--- ask_ollama.py
+++ ask_ollama.py
@@ -12,13 +12,17 @@ monkey.patch_all()
 import os
 import re
 import sys
-import json
 import logging
 import argparse
 from os import path
 
 import requests
 from gevent.pool import Pool
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+}
+
 
 def setup_logging(lv):
     logger = logging.getLogger()
@@ -28,42 +32,50 @@ def setup_logging(lv):
     logger.setLevel(lv)
 
 
-def get_text_from_url(url, ignore_status=False):
+def get_text_from_url(url, raise_status=False):
     from bs4 import BeautifulSoup
     logging.info(f'get content from url: {url}')
-    resp = requests.get(url)
-    resp.raise_for_status()
+    resp = requests.get(url, headers=headers)
+    if raise_status:
+        resp.raise_for_status()
+    elif resp.status_code >= 400:
+        logging.error(f'error when get content from url: {url} {resp.status_code}')
+        return
     doc = BeautifulSoup(resp.text, 'html.parser')
-    return doc.get_text(" ", strip=True)
+    return doc.get_text('\n', strip=True)
 
 
-def ask_startpage(query):
-    from bs4 import BeautifulSoup
-    resp = requests.post('https://www.startpage.com/sp/search', data={'query': query, 't': 'device'})
-    resp.raise_for_status()
-    doc = BeautifulSoup(resp.text, 'html.parser')
-    for a in doc.find_all('a'):
-        print(a)
+def get_text_from_urls(urls, concurrent=5, raise_status=False):
+    pool = Pool(concurrent)
+    docs = pool.imap(lambda u: get_text_from_url(u, raise_status), urls)
+    return [doc for doc in docs if doc]
 
 
-def duckduckgo(q):
+def duckduckgo(q, max_results=5):
     from duckduckgo_search import DDGS
-    results = DDGS().text(q, max_results=args.search_engine_max_results)
+    results = DDGS().text(q, max_results=max_results)
     return [r['href'] for r in results]
 
 
-def ask_search_engine(command, search_engine):
-    results = search_engine(command)
-    pool = Pool(args.search_engine_max_results)
-    def f(r):
-        try:
-            return get_text_from_url(r)
-        except:
-            return
-    return [doc for doc in pool.imap(f, results) if doc]
+def ollama_chat(messages, remove_think=False):
+    logging.info(f'send request to ollama: {args.endpoint} {args.model}')
+    resp = requests.post(f'{args.endpoint}/api/chat', json={
+        'model': args.model,
+        'stream': False,
+        'messages': messages,
+        'options': options,
+    })
+    resp.raise_for_status()
+    logging.info('received response from ollama')
 
-
-re_think = re.compile('<think>.*</think>', re.DOTALL)
+    data = resp.json()
+    logging.info(fmt_stat(data))
+    response = data['message']['content']
+    if args.debug:
+        logging.debug(f'response: {response}')
+    if remove_think:
+        response = re_think.sub('', response)
+    return response
 
 
 def fmt_stat(data):
@@ -75,6 +87,9 @@ def fmt_stat(data):
     eval_duration = data['eval_duration'] / 10**9
     eval_rate = eval_count / eval_duration
     return f'total_duration: {duration_total:.2f}, prompt_eval_count: {prompt_eval_count}, prompt_eval_duration: {prompt_eval_duration:.2f}, eval_count: {eval_count}, eval_duration: {eval_duration:.2f}, eval_rate: {eval_rate:.2f}'
+
+
+re_think = re.compile('<think>.*</think>', re.DOTALL)
 
 
 def main():
@@ -89,8 +104,8 @@ def main():
     parser.add_argument('--remove-think', '-rt', action='store_true', help='remove think')
     parser.add_argument('--file', '-f', action='append', help='input file')
     parser.add_argument('--url', '-u', action='append', help='source url')
-    parser.add_argument('--search-duckduckgo', '-sd', action='store_true', help='search duckduckgo as source')
-    parser.add_argument('--search-engine-max-results', '-semr', type=int, default=3, help='max results for refer')
+    parser.add_argument('--search-duckduckgo', '-ddgs', action='store_true', help='search duckduckgo as source')
+    parser.add_argument('--search-engine-max-results', '-semr', type=int, default=5, help='max results for refer')
     parser.add_argument('rest', nargs='*', type=str)
     args = parser.parse_args()
 
@@ -104,55 +119,44 @@ def main():
 
     command = '. '.join(args.rest)
     if not command:
-        command = '给代码写注释'
+        logging.error('no command')
+        return
     if args.debug:
         logging.debug(f'command: {command}')
 
     background = []
+
     if args.file:
         for fp in args.file:
             with open(fp) as fi:
                 logging.info(f'get content from file: {fp}')
                 background.append(fi.read())
+
     if args.url:
-        for u in args.url:
-            background.append(get_text_from_url(u))
+        background.extend(get_text_from_urls(args.url, len(args.url), raise_status=True))
+
     if args.from_input:
         background.append(sys.stdin.read())
+
     if args.search_duckduckgo:
-        background.extend(ask_search_engine(command, duckduckgo))
+        urls = duckduckgo(command, args.search_engine_max_results)
+        for u in urls:
+            logging.info(f'search result url: {u}')
+        background.extend(get_text_from_urls(urls, args.search_engine_max_results))
 
     if args.debug:
         for b in background:
             logging.debug(f'background: {b}')
 
-    background = ['<start>%s</end>' % doc for doc in background]
     messages = [
-        {
-            'role': 'system',
-            'content': '你是一个AI个人助理，请阅读以下材料，帮助用户回答问题。每篇材料以<start>开始，以</end>结束。\n'+'\n'.join(background)
-        },
-        {'role': 'user', 'content': command}
+        {'role': 'system', 'content': '你是一个AI个人助理，请阅读以下材料，帮助用户回答问题。每篇材料以<start>开始，以</end>结束。回答问题的时候，需要给出每篇材料的原始信息引用和位置。'}
     ]
+    for doc in background:
+        messages.append({'role': 'user', 'content': f'<start>{doc}</end>'})
+    messages.append({'role': 'user', 'content': command})
 
-    logging.info(f'send request to ollama: {args.endpoint} {args.model}')
-    resp = requests.post(f'{args.endpoint}/api/chat', json={
-        'model': args.model,
-        'stream': False,
-        'messages': messages,
-        'options': options,
-    })
-    resp.raise_for_status()
-    logging.info('received response from ollama')
-
-    data = resp.json()
-    if args.debug:
-        logging.debug(f'response: {json.dumps(data, indent=2)}')
-    response = data['message']['content']
-    if args.remove_think:
-        response = re_think.sub('', response)
+    response = ollama_chat(messages, args.remove_think)
     print(response)
-    logging.info(fmt_stat(data))
 
 
 if __name__ == '__main__':
