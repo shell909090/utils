76b3e97d3aa9c35deeb006c3e5fe478459bbdcb6
Shell.Xu <shell@testany.io>
2025-04-03 17:43:19 +0800

    add support of openai in ask_ollamma, and rename it to ask ai

--- ask_ai.py
+++ ask_ai.py
@@ -19,6 +19,9 @@ from os import path
 import requests
 from gevent.pool import Pool
 
+# import http.client as http_client
+# http_client.HTTPConnection.debuglevel = 1
+
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
 }
@@ -57,28 +60,7 @@ def duckduckgo(q, max_results=5):
     return [r['href'] for r in results]
 
 
-def ollama_chat(messages, remove_think=False):
-    logging.info(f'send request to ollama: {args.endpoint} {args.model}')
-    resp = requests.post(f'{args.endpoint}/api/chat', json={
-        'model': args.model,
-        'stream': False,
-        'messages': messages,
-        'options': options,
-    })
-    resp.raise_for_status()
-    logging.info('received response from ollama')
-
-    data = resp.json()
-    logging.info(fmt_stat(data))
-    response = data['message']['content']
-    if args.debug:
-        logging.debug(f'response: {response}')
-    if remove_think:
-        response = re_think.sub('', response)
-    return response
-
-
-def fmt_stat(data):
+def fmt_ollama_stat(data):
     # 将所有浮点数输出改为小数点后两位格式
     duration_total = data['total_duration'] / 10**9
     prompt_eval_count = data['prompt_eval_count']
@@ -89,7 +71,54 @@ def fmt_stat(data):
     return f'total_duration: {duration_total:.2f}, prompt_eval_count: {prompt_eval_count}, prompt_eval_duration: {prompt_eval_duration:.2f}, eval_count: {eval_count}, eval_duration: {eval_duration:.2f}, eval_rate: {eval_rate:.2f}'
 
 
+def ollama_chat(messages):
+    logging.info(f'send request to ollama: {args.ollama_endpoint} {args.model}')
+    resp = requests.post(f'{args.ollama_endpoint}/api/chat', json={
+        'model': args.model,
+        'stream': False,
+        'messages': messages,
+        'options': options,
+    })
+    resp.raise_for_status()
+    logging.info('received response from ollama')
+    data = resp.json()
+    logging.info(fmt_ollama_stat(data))
+    return data['message']['content']
+
+
+def fmt_openai_stat(usage):
+    return f"total_tokens: {usage['total_tokens']}, prompt_tokens: {usage['prompt_tokens']}, completion_tokens: {usage['completion_tokens']}"
+
+
+def openai_chat(messages):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    if args.openai_apikey:
+        headers['Authorization'] = f'Bearer {args.openai_apikey}'
+    logging.info(f'send request to openai: {args.openai_endpoint} {args.model}')
+    resp = requests.post(f'{args.openai_endpoint}/chat/completions', headers=headers, json={
+        'model': args.model,
+        'stream': False,
+        'messages': messages,
+    })
+    resp.raise_for_status()
+    logging.info('received response from openai')
+    data = resp.json()
+    logging.info(fmt_openai_stat(data['usage']))
+    return data['choices'][0]['message']['content']
+
 re_think = re.compile('<think>.*</think>', re.DOTALL)
+def ai_chat(messages, remove_think=False):
+    if args.ollama_endpoint:
+        response = ollama_chat(messages)
+    else:
+        response = openai_chat(messages)
+    if args.debug:
+        logging.debug(f'response: {response}')
+    if remove_think:
+        response = re_think.sub('', response)
+    return response
 
 
 def main():
@@ -97,8 +126,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', '-d', action='store_true', help='debug mode')
     parser.add_argument('--log-level', '-l', default='INFO', help='log level')
-    parser.add_argument('--endpoint', '-e', default=os.getenv('OLLAMA_ENDPOINT', 'http://127.0.0.1:11434'), help='ollama endpoint')
-    parser.add_argument('--model', '-m', default=os.getenv('CHAT_MODEL', 'deepseek-r1:14b'), help='ollama model')
+    parser.add_argument('--ollama-endpoint', '-ae', default=os.getenv('OLLAMA_ENDPOINT'), help='ollama endpoint')
+    parser.add_argument('--openai-endpoint', '-ie', default=os.getenv('OPENAI_ENDPOINT'), help='openai endpoint')
+    parser.add_argument('--openai-apikey', '-ik', default=os.getenv('OPENAI_APIKEY'), help='openai apikey')
+    parser.add_argument('--model', '-m', default=os.getenv('CHAT_MODEL', 'deepseek-r1:14b'), help='model')
     parser.add_argument('--from-input', '-fi', action='store_true', help='read background from stdin')
     parser.add_argument('--max-context-length', '-c', type=int, default=32768, help='maximum context length')
     parser.add_argument('--remove-think', '-rt', action='store_true', help='remove think')
@@ -110,6 +141,9 @@ def main():
     args = parser.parse_args()
 
     setup_logging(args.log_level.upper())
+
+    if not args.ollama_endpoint and not args.openai_endpoint:
+        args.ollama_endpoint = 'http://127.0.0.1:11434'
 
     global options
     options = {
@@ -155,7 +189,7 @@ def main():
         messages.append({'role': 'user', 'content': f'<start>{doc}</end>'})
     messages.append({'role': 'user', 'content': command})
 
-    response = ollama_chat(messages, args.remove_think)
+    response = ai_chat(messages, args.remove_think)
     print(response)
 
 
