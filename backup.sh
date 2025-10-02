d28180a9e93c119ab52c5db2e50db680fb8a794d
shell909090 <shell909090@gmail.com>
2025-10-02 17:37:47 +0000

    update backup.sh to support sudo with password

--- backup.sh
+++ backup.sh
@@ -5,17 +5,30 @@ if [[ "$TGT" == *.tar.gz.gpg ]]; then
     TGT=${TGT::-11}
 fi
 rm -f "$TGT.tar.gz.gpg"
-TMPDIR=`ssh "$TGT" "mktemp -d"`
-echo "create tempdir $TMPDIR"
+
+read -s -p "Enter your password: " PASSWORD
+echo ''
+
+echo "create backup."
 ssh "$TGT" <<EOF
-cd $TMPDIR
+pushd .
+TMPDIR=\$(mktemp -d)
+cd \$TMPDIR
 apt-mark showmanual > apt-mark-manual.lst
 dpkg -l > dpkg.lst
-tar uf backup.tar apt-mark-manual.lst dpkg.lst ~/service ~/configs
-sudo bash -c 'while read i; do tar uf backup.tar \$i; done' < ~/configs
+tar uf backup.tar apt-mark-manual.lst dpkg.lst ~/service ~/configs > /dev/null
+FULLPATH=\$(realpath ~/configs)
+echo $PASSWORD | sudo -S bash -c "while read i; do tar uf backup.tar \\\$i; done < \$FULLPATH"
 test -x ~/backup.sh && tar uf backup.tar ~/backup.sh
 test -x ~/backup.sh && ~/backup.sh
-sudo chown $SUDO_UID:$SUDO_GID backup.tar
+echo $PASSWORD | sudo -S chown \$SUDO_UID:\$SUDO_GID backup.tar
+gzip -c backup.tar > ~/backup.tar.gz
+popd
+rm -rf \$TMPDIR
 EOF
-ssh "$TGT" "gzip -c $TMPDIR/backup.tar" | gpg -e -r 0xC9A514BA45DE0475! > "$TGT.tar.gz.gpg"
-ssh "$TGT" "rm -rf \"$TMPDIR\""
+
+echo "copy backup back"
+ssh "$TGT" 'cat backup.tar.gz' | gpg -e -r 0xC9A514BA45DE0475! > "$TGT.tar.gz.gpg"
+
+echo "cleanup remote"
+ssh "$TGT" 'rm -rf ~/backup.tar.gz'
